8c264c8d6e2d7aad9f27caf614060c9561fea307
Tyler Mace <tylerdmace@gmail.com>
2015-02-25 16:49:37 -0700

    removed isolation function and changed reference 'this' to 'mongeese'

--- mongeese.js
+++ mongeese.js
@@ -1,48 +1,46 @@
 'use strict';
 
-(function() {
-  var mongoose = require('mongoose'),
-      mongeese;
+var mongoose = require('mongoose'),
+    mongeese;
 
-  module.exports = mongeese = {
-    // Our list of current connections
-    connections: {},
+module.exports = mongeese = {
+  // Our list of current connections
+  connections: {},
 
-    // Closing an existing connection
-    closeConnection: function (name) {
-      if (name !== undefined) {
-        this.connections[name].base.disconnect();
-      } else { throw new Error('Connection name required.'); }
-    },
+  // Closing an existing connection
+  closeConnection: function (name) {
+    if (name !== undefined) {
+      mongeese.connections[name].base.disconnect();
+    } else { throw new Error('Connection name required.'); }
+  },
 
-    // Getting an already established connection
-    getConnection: function (name) {
-      if (name !== undefined) {
-        return this.connections[name];
-      } else { throw new Error('Connection name required.'); }
-    },
+  // Getting an already established connection
+  getConnection: function (name) {
+    if (name !== undefined) {
+      return mongeese.connections[name];
+    } else { throw new Error('Connection name required.'); }
+  },
 
-    // Creating and returning a new connection
-    createConnection: function (name, uri) {
-      if (name !== undefined) {
-        this.connections[name] = mongoose.createConnection(uri || 'mongodb://localhost/test');
-        return this.connections[name];
-      } else { throw new Error('Connection name required.'); }
-    },
+  // Creating and returning a new connection
+  createConnection: function (name, uri) {
+    if (name !== undefined) {
+      mongeese.connections[name] = mongoose.createConnection(uri || 'mongodb://localhost/test');
+      return mongeese.connections[name];
+    } else { throw new Error('Connection name required.'); }
+  },
 
-    // List all current connections
-    listAllConnections: function () {
-      return this.connections;
-    },
+  // List all current connections
+  listAllConnections: function () {
+    return mongeese.connections;
+  },
 
-    // Close all current connections
-    closeAllConnections: function () {
-      try {
-        for (var connection in this.connections) {
-          this.connections[connection].base.disconnect();
-        }
-        return true;
-      } catch (err) { throw new Error('Unable to close connections: ' + err); }
-    }
-  };
-}).call(this);
+  // Close all current connections
+  closeAllConnections: function () {
+    try {
+      for (var connection in this.connections) {
+        mongeese.connections[connection].base.disconnect();
+      }
+      return true;
+    } catch (err) { throw new Error('Unable to close connections: ' + err); }
+  }
+};
