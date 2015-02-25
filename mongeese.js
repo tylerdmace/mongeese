67d9f6ec7e8e0b8d991460b119eb37af90a211e8
Tyler Mace <tylerdmace@gmail.com>
2015-02-25 01:49:53 -0700

    first real commit

---
+++ mongeese.js
@@ -0,0 +1,48 @@
+'use strict';
+
+(function() {
+  var mongoose = require('mongoose'),
+      mongeese;
+
+  module.exports = mongeese = {
+    // Our list of current connections
+    connections: {};
+
+    // Closing an existing connection
+    closeConnection: function (name) {
+      if (name !== undefined) {
+        this.connections[name].disconnect();
+      } else { throw new Error('Connection name required.'); }
+    };
+
+    // Getting an already established connection
+    getConnection: function (name) {
+      if (name !== undefined) {
+        return this.connections[name];
+      } else { throw new Error('Connection name required.'); }
+    };
+
+    // Creating and returning a new connection
+    createConnection: function (name, uri) {
+      if (name !== undefined) {
+        this.connections[name] = this.createConnection(uri || 'mongodb://localhost/test');
+        return this.connections[name];
+      } else { throw new Error('Connection name required.'); }
+    };
+
+    // List all current connections
+    listAllConnections: function () {
+      return this.connections;
+    };
+
+    // Close all current connections
+    closeAllConnections: function () {
+      try {
+        for (var connection in this.connections) {
+          this.connections[connection].base.disconnect();
+        }
+        return true;
+      } catch (err) { throw new Error('Unable to close connections: ' + err); }
+    };
+  };
+}).call(this);
